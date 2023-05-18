ae5255473d068ba5719d322534785f98d8a1ec84
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-05-18 19:29:40 +0000

    Vistas estaticas👓

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,6 +2,7 @@
 import { Router } from 'express';
 // Creando una instancia del enrutador de express
 const router = Router();
+import path from 'path';
 // GET /add-product
 router.get('/add-product', (req, res, next) => {
   // Si la petición es post pasamos el siguiente
@@ -9,12 +10,7 @@ router.get('/add-product', (req, res, next) => {
   if(req.method === "POST") return next();
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  res.send(`
-  <form action="/add-product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add product</button>
-  </form>
-  `);
+  res.sendFile(path.resolve('views','add-product.html'));
 });
 // POST /add-product
 router.post('/add-product', (req, res) => {
--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -2,14 +2,14 @@
 import { Router } from 'express';
 // Creando una instancia del enrutador de express
 const router = Router();
+//importandi enrutador
+import path from 'path';
+
 // GET ///middleware de respuesta(middleware final)
 router.get('/', (req, res)=>{
   console.log("📢 Sirviendo la ruta '/'");
   // Se contesta al server
-  res.send(`
-  <h1> Bienvenido a express</h1>
-  <p>Esta es mi app asombrosa</p>
-  `);
+  res.sendFile(path.resolve('views','shop.html'));
 });
 // GET /about///middleware de proposito especifico//about
 router.get('/about', (req, res) => {
