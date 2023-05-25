8c5022fe12cc3b2425c86a52dc3310f34de4d00e
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-05-25 14:24:17 +0000

    estilos

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -3,6 +3,8 @@ import { Router } from 'express';
 // Creando una instancia del enrutador de express
 const router = Router();
 import path from 'path';
+//importando ROOT_DIR
+import { ROOT_DIR } from './helpers/paths.js';
 // GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
   // Si la petición es post pasamos el siguiente
@@ -10,7 +12,8 @@ router.get('/add-product', (req, res, next) => {
   if(req.method === "POST") return next();
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  res.sendFile(path.resolve('views','add-product.html'));
+  console.log(`ROOT_DIR:${ROOT_DIR}`);
+  res.sendFile(path.join(ROOT_DIR,'views','add-product.html'));
 });
 // POST /add-product
 router.post('/add-product', (req, res) => {
---
+++ routes/helpers/paths.js
@@ -0,0 +1,6 @@
+import path from 'path';
+//mediante esta linea de codigo
+//se asigna a Root_dir la ruta
+//de la raiz del proyecto
+
+export const ROOT_DIR = path.resolve();
